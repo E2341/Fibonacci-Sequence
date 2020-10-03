e9d4bcd2cb3f33b13ef8bc694fcd8c6098152943
E2341 <67653460+E2341@users.noreply.github.com>
2020-10-04 00:48:10 +0200

    Fibonacci Sequence created...

---
+++ myScript.js
@@ -0,0 +1,14 @@
+document.getElementById("btn").addEventListener(
+    "click", fibonacci);
+function fibonacci() {
+    const n = document.getElementById("number").value;
+    var fibo = [1, 1];
+    for (i = 0; i < n-2; i++) {
+        fibo.push(fibo[i] + fibo[i+1]);
+    }
+    if (isNaN(n) || n < 1) {
+        document.getElementById("result").innerHTML = "Please check your input!";
+    } else {
+        document.getElementById("result").innerHTML = n + ". element of Fibonacci Sequence is " + fibo[n-1];
+    }
+}
